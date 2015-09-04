959a140428435f0d0c048b1c14325b571f961f55
Philip Sugg <pbensugg@gmail.com>
2015-09-03 23:45:20 -0500

    add working api connection with incandescent in controllers-nethttp.rb

--- app/controllers/searches.rb
+++ app/controllers/searches.rb
@@ -11,17 +11,18 @@ uid = "6609"
 apikey = "030a0348079fdd2aa3309e6cc79a8bcd"
 
 # Expires - when the signature will become invalid (UNIX timestamp) - may be no more than 1200 seconds from now.
-expires = "1441313473"#DateTime.now+1190
+expires = 1441313473#DateTime.now+1190
 
 # Generate Signature
-stringToSign = uid+"\n"+expires
+stringToSign = "#{uid}\n#{expires}"
 binarySignature = OpenSSL::HMAC.digest('sha1', apikey, stringToSign)
 # puts "binary sig: #{binarySignature}"
-signature = URI.escape(Base64.encode64(binarySignature));
+signature = CGI.escape(Base64.strictencode64(binarySignature));
 # p signature
-data = {"uid"=>uid,"expires"=>expires,"signature"=>signature}
 # p data
 # p data.to_json
+
+"gCvc/zhTP2nCJy756Um/B/Dwd+I%3D"
 
 # ^^^^^^^^^^^^^^^^^ END OF SIGNATURE CODE ^^^^^^^^^^^^^
 
@@ -30,12 +31,12 @@ data = {"uid"=>uid,"expires"=>expires,"signature"=>signature}
 
 post "/searches" do
   url = params[:source_img_url]
-  data = { uid: uid, expires: expires, signature: signature, images: url }
+  data = { uid: uid, expires: expires, signature: signature, images: [url] }
 
   content_type :json
   jsonObj = data.to_json
 
-  uri = URI("https://incandescent.xyz/api/add/")
+  uri = URI("https://incandescent.  xyz/api/add/")
 
   request = Net::HTTP::Post.new("https://incandescent.xyz/api/add/", initheader = {'Content-Type' => 'application/json'} )
   request.body = jsonObj
